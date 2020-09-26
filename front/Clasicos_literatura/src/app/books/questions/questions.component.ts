82546c3dced2c9a42d406c21b7c8554bafeea714
Sebastián Díaz Gaona <sediazga@poligran.edu.co>
2020-09-25 21:37:17 -0500

    quiz almost compelte funcional and test in slides

--- front/Clasicos_literatura/src/app/books/questions/questions.component.ts
+++ front/Clasicos_literatura/src/app/books/questions/questions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-questions',
@@ -10,50 +11,62 @@ export class QuestionsComponent implements OnInit {
 
   choosed:string;
 
-  alphas = ["1","2","3"]
-  bethas:string[] = ["Cómo", "Qué", "Quién"] 
-  tetha: string=this.bethas[0]; 
-  answers:number[]=[];
-  score:number=0;
+  alphas = ["Answer 1","Answer 2","Answer 3"]
 
-  constructor(public alertController: AlertController) { }
+  bethas:string[] = ["Cómo", "Qué", "Quién"]
+
+  respuestas:string[]=["choosed1","choosed2","choosed3"];
+  tetha: string=this.bethas[0]; 
+
+  answers:string[]=[];
+  
+
+  constructor(public alertController: AlertController,private router: Router) { }
 
   ngOnInit() {
   }
 
-
-  verifyQuiz(){
-    this.alphas.forEach(element => {
-      
-    });
-  }
-
-  async presentAlertConfirm() {
+  async presentAlertConfirm(score:number) {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Finalizado',
-      message: 'Su puntaje es: '+this.score,
+      message: 'Su puntaje es: '+score,
       buttons: [
          {
           text: 'Okay',
           handler: () => {
-            console.log('Confirm Okay');
+            this.router.navigate(['/']);
+            this.answers=[];
           }
         }
       ]
     });
-
     await alert.present();
   }
 
-
-  add(){
-    console.log(this.choosed)
-    if(this.bethas.indexOf(this.tetha)<this.bethas.length){
-      this.tetha=this.bethas[this.bethas.indexOf(this.tetha)+1]
-    }else if(this.bethas.indexOf(this.tetha)<0){
-      this.presentAlertConfirm();
+  //verify answers
+  verifyAnswers(){
+    let score=0;
+    for (let i = 0; i < this.respuestas.length; i++) {
+      if(this.answers[i]==this.respuestas[i]){
+        score=score+1;
+      }
     }
-    console.log(this.bethas.indexOf(this.tetha));
+    this.presentAlertConfirm(score);
+  }
+
+
+  //push the selected option if the quiz is not end, and change question
+  //at end verify answers and reset
+  add(){
+    if(this.bethas.indexOf(this.tetha)<this.bethas.length){
+      this.answers.push(this.choosed);
+      this.tetha=this.bethas[this.bethas.indexOf(this.tetha)+1]
+      this.choosed=null;
+      if(this.bethas.indexOf(this.tetha)==-1){
+        this.verifyAnswers();
+        this.tetha=this.bethas[0];
+      }
+    }
   }
 }
