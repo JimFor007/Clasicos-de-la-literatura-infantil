3017b082684bdfc3e3f93bc8d3b6bfce9a2990c9
angarcia20 <angarcia20@poligran.edu.co>
2020-10-31 17:18:00 -0500

    iconos favoritos

--- front/Clasicos_literatura/src/app/books/allbooks/allbooks.page.ts
+++ front/Clasicos_literatura/src/app/books/allbooks/allbooks.page.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { book } from 'src/app/models/libro.model';
 import { AlertController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { userFavorite } from 'src/app/models/userFavorite.model';
 @Component({
   selector: 'app-allbooks',
   templateUrl: './allbooks.page.html',
@@ -20,6 +21,11 @@ export class AllbooksPage implements OnInit {
   users: any[]=[];
   books: any[]=[];
   id: string;
+  apunte:string;
+
+  booksFav: any[] = [];
+
+
   constructor(private testService: TestService, private router: Router, public alertController: AlertController) { 
   }
 
@@ -27,10 +33,19 @@ export class AllbooksPage implements OnInit {
     this.testService.getTodos().subscribe(
       data=>{
         this.authors= data;
+        console.log(this.authors);
       });
       this.testService.stateUser().subscribe(id=>{
+        if(id===null){this.id=null}
+        else{
         this.id=id.uid;
-      });
+        this.testService.getUserData(this.id).subscribe(
+          favorites =>{
+            this.booksFav=  favorites.libros;
+          }
+        )
+      }
+    });
       
     }
 
@@ -38,8 +53,40 @@ export class AllbooksPage implements OnInit {
     this.testService.añadirLibro(this.id,titulo, imagen)
     this.presentAlertConfirm();
   }
+
   search(event){
     this.textoBuscar=event.detail.value
+  }
+  async unFavConfirm(titulo:string,imagen:string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: "¿Esta seguro que desea eliminar este libro de favoritos?",
+      message: 'se eliminara <strong>'+titulo+'</strong> de sus favoritos',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        },
+         {
+          text: 'Okay',
+          handler: () => {  
+            
+            this.booksFav.find(booksFav=> {
+             if( booksFav.titulo=== titulo){
+             this.testService.eliminarLibro(this.id,titulo,imagen,booksFav.apunte);
+             }    
+            }
+              );
+            
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   async presentAlertConfirm() {
@@ -64,6 +111,19 @@ export class AllbooksPage implements OnInit {
   }
   read(book: string){
     this.router.navigate(["/lecturalibro",book]);
+  }
+
+  stateicon(nombre: string){
+    
+   if(this.booksFav.find(booksFav=> booksFav.titulo   === nombre) === undefined){
+    return 0;
+   }
+   else{
+     return 1;
+   }
+
+
+
   }
 
 }
