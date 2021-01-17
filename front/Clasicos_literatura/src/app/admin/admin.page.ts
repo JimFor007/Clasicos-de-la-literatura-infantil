fa3f383d4b351b7db546f0b19bf471bc55d5038d
Jimmy Giovanny Forero Forero <jiforero2@poligran.edu.co>
2021-01-16 19:55:01 -0500

    Add books, authors, chapters...

--- front/Clasicos_literatura/src/app/admin/admin.page.ts
+++ front/Clasicos_literatura/src/app/admin/admin.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,Validators } from '@angular/forms';
-import {TestService} from '../Services/test.service';
+import { TestService } from '../Services/test.service';
 import { author } from '../models/author.model';
 @Component({
   selector: 'app-admin',
@@ -8,41 +7,41 @@ import { author } from '../models/author.model';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-  authors:author[] = [];
+  authors: author[] = [];
 
   autor = {
     autorName: '',
-    image:'',
+    image: '',
     bio: '',
-    birthDate:'',
-    books:[{
-        titulo:'',
-        capitulos:[{
-          titulo:''
-        },{
-          titulo:''
-        }]
-      },
-      {
-
+    birthDate: '',
+    books: [{
+      titulo: '',
+      capitulos: [{
+        titulo: ''
+      }, {
+        titulo: ''
       }]
-    }
+    },
+    {
 
-  
+    }]
+  }
+
+
   constructor(private testService: TestService) {
 
-   }
+  }
 
   ngOnInit() {
     this.testService.getTodos().subscribe(
-      data=>{
-        this.authors= data;
+      data => {
+        this.authors = data;
       });
-    }
-  
-    onSubmitTemplate(){
+      
+  }
+
+  onSubmitTemplate() {
     console.log('Form submit')
-    console.log(this.autor)
     this.testService.añadirAutor(this.autor.autorName, this.autor.image, this.autor.bio, this.autor.birthDate);
   }
 }
